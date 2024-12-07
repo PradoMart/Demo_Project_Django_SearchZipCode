47c3f0c1b1be4df3694b9d2f1209aeb035305670
Leandro <leandroudiprado@gmail.com>
2024-12-07 12:05:10 -0300

    creating the logical of this program in web/view.py

--- consulta_cep/web/views.py
+++ consulta_cep/web/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def consulta_cep(request):
+    dados = None
+    erro = None
+
+    if request.method == 'POST':
+        cep = request.POST.get('cep')
+        if not cep:
+            erro = 'Por favor, insira um CEP válido.'
+        else:
+            url = f'https://brasilapi.com.br/api/cep/v1/{cep}'
+            
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                dados = response.json()
+            except requests.exceptions.RequestException:
+                erro = 'Erro ao consultar o CEP. Verifique e tente novamente.'
+    return render(request,'consulta_cep.html',{'dados': dados, 'erro': erro})
